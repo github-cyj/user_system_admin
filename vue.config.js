f9f3a6efc6e49373ee093ed6a7690418e761ff84
xa_caoyj <xa_caoyj@xiannew.sunseer.co.jp>
2024-01-05 18:12:52 +0800

    fix 上服务器出现的问题

--- vue.config.js
+++ vue.config.js
@@ -30,13 +30,15 @@ module.exports = {
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
+    allowedHosts: ['.51job.top'],
+    host: '0.0.0.0',
     port: port,
     open: true,
     overlay: {
       warnings: false,
       errors: true
-    },
-    before: require('./mock/mock-server.js')
+    }
+    // before: require('./mock/mock-server.js')
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
@@ -88,7 +90,7 @@ module.exports = {
             .plugin('ScriptExtHtmlWebpackPlugin')
             .after('html')
             .use('script-ext-html-webpack-plugin', [{
-            // `runtime` must same as runtimeChunk name. default is `runtime`
+              // `runtime` must same as runtimeChunk name. default is `runtime`
               inline: /runtime\..*\.js$/
             }])
             .end()
