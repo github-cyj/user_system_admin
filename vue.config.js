a8b50f4339cd8f52267ce6dc1dd13c5a6adf048e
xa_caoyj <xa_caoyj@sunseer.co.jp>
2024-03-01 16:31:35 +0800

    添加新的的本地配置

--- vue.config.js
+++ vue.config.js
@@ -30,7 +30,7 @@ module.exports = {
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
-    allowedHosts: ['.51job.top'],
+    allowedHosts: ['.51job.top', '.local'],
     host: '0.0.0.0',
     port: port,
     open: true,
